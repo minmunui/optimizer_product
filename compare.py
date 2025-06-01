400a4a0a13f0643d51034ad0d25228c9d6e70340
DongjinPark <ehdwls1638@pusan.ac.kr>
2025-06-01 20:57:49 +0900

    refactor: compare.py의 제약변수 리팩터링

--- compare.py
+++ compare.py
@@ -4,13 +4,15 @@
 """
 
 import pandas as pd
+import os
 
 import src.solver.cpsat as cpsat
 import src.solver.scip as scip
 from src.problem.io import add_nothing_strategy
 from src.problem.strategy import make_random_problem
 PROBLEM_COST_COEF = 1_000  # 문제 비용 계수
-for n_item in [10]:
+
+for n_item in [50]:
 
     total_elapsed_time = []
     total_obj = []
@@ -30,6 +32,8 @@ for n_item in [10]:
         problem_result = []
         problem_values = []
         problem_elapsed_times = []
+        cost_constraint = 0.5 * n_item * PROBLEM_COST_COEF
+        reliability_constraint = [2.5 * n_item, 2.5 * n_item, 2.5 * n_item]
 
         for allow_zero_strategy in [True, False]:
             print(f"=========================== ALLOW ZERO STRATEGY {allow_zero_strategy} ==========================")
@@ -39,7 +43,7 @@ for n_item in [10]:
 
             print(f" ------------------- SCIP Cost Constraint: {i} ------------------- ")
             sol_scip_cost, total_cost, total_value, elapsed_time = (
-                scip.solve_cost_constraint(problem, cost_constraint=0.5 * n_item * PROBLEM_COST_COEF, value_weights=None,
+                scip.solve_cost_constraint(problem, cost_constraint=cost_constraint, value_weights=None,
                                            allow_zero_strategy=allow_zero_strategy))
             print(f"Selected: {sol_scip_cost}")
             print(f"Total Cost: {total_cost}")
@@ -51,7 +55,7 @@ for n_item in [10]:
 
             print(f" ------------------- CP-SAT Cost Constraint: {i} ------------------- ")
             sol_cpsat_cost, total_cost, total_value, elapsed_time = (
-                cpsat.solve_cost_constraint(problem, cost_constraint=0.5 * n_item * PROBLEM_COST_COEF, value_weights=None,
+                cpsat.solve_cost_constraint(problem, cost_constraint=cost_constraint, value_weights=None,
                                             allow_zero_strategy=allow_zero_strategy))
             print(f"Selected: {sol_cpsat_cost}")
             print(f"Total Cost: {total_cost}")
@@ -64,7 +68,7 @@ for n_item in [10]:
             print(f" ------------------- SCIP Reliability Constraint: {i} ------------------- ")
             sol_scip_reliability, total_cost, total_value, elapsed_time = (
                 scip.solve_reliability_constraint(problem,
-                                                  reliability_constraint=[2.5 * n_item, 2.5 * n_item, 2.5 * n_item],
+                                                  reliability_constraint=reliability_constraint,
                                                   allow_zero_strategy=allow_zero_strategy))
             print(f"Selected: {sol_scip_reliability}")
             print(f"Total Cost: {total_cost}")
@@ -77,7 +81,7 @@ for n_item in [10]:
             print(f" ------------------- CP-SAT Reliability Constraint: {i} ------------------- ")
             sol_cpsat_reliability, total_cost, total_value, elapsed_time = (
                 cpsat.solve_reliability_constraint(problem,
-                                                   reliability_constraint=[2.5 * n_item, 2.5 * n_item, 2.5 * n_item],
+                                                   reliability_constraint=reliability_constraint,
                                                    allow_zero_strategy=allow_zero_strategy))
             print(f"Selected: {sol_cpsat_reliability}")
             print(f"Total Cost: {total_cost}")
@@ -90,8 +94,8 @@ for n_item in [10]:
             if allow_zero_strategy:
                 problem = add_nothing_strategy(problem)
 
-        problem_result.append(2 * n_item)  # cost constraint
-        problem_result.append([2.5 * n_item, 2.5 * n_item, 2.5 * n_item])
+        problem_result.append(cost_constraint)  # cost constraint
+        problem_result.append(reliability_constraint)
         for i in problem_result:
             print(i)
         total_elapsed_time.append(problem_elapsed_times)
